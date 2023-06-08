18a185d4d9ed8520380a5755f769ab0f65272da1
Admin <donwilsonobido@gmail.com>
2023-06-08 19:28:22 +0800

    add argocd.tf file

---
+++ 15-argocd.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "2.3.5"
+}
+# helm install argocd -n argocd -f values/argocd.yaml
